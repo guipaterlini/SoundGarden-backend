9e6219bd1363598523a637481ddf20ca27a47466
guipaterlini <guilhermepaterlini@gmail.com>
2023-03-06 22:38:54 -0400

    Feat: completada a rota de inser event, find all

--- src/server.js
+++ src/server.js
@@ -1,8 +1,11 @@
 import express from "express";
+import { randomUUID } from "node:crypto";
 
 const app = express();
 
 const PORT = 3333;
+
+app.use(express.json()); //sempre em cima das rotas
 
 var eventos = [];
 
@@ -11,26 +14,33 @@ app.post("/events", (req, res) => {
   const { name, poster, attractions, description, scheduled, number_tickets } =
     req.body;
 
-  eventos.push({
+  const evento = {
+    _id: randomUUID(),
     name,
     poster,
     attractions,
     description,
     scheduled,
     number_tickets,
-  });
+  };
+
+  eventos.push(evento);
 
   return res.status(201).json({ eventos });
 });
 
 // FIND ALL EVENTS
 app.get("/events", (req, res) => {
-  return res.status(200).json({ message: "sucess" });
+  return res.status(200).json({ eventos });
 });
 
 // FIND ONE EVENT
 app.get("/events/:_id", (req, res) => {
-  return res.status(200).json({ message: "sucess" });
+  const { _id } = req.params;
+
+  const evento = eventos.find((evento) => evento._id === _id);
+
+  return res.status(200).json({ evento });
 });
 
 // UPDATE EVENT
